8318264e4f5c76edfcf028a0ee329b85a11b7f47
lpjfalcao <lpjfalcao@gmail.com>
2025-12-09 16:04:41 -0300

    Criando a estrutura do projeto e implementando a header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+import { HeaderComponent } from './shared/header/header.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,
+     HeaderComponent,
+     MatSlideToggleModule,
+     MatIconModule,
+     MatDividerModule, 
+     MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
