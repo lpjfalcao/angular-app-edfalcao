98699133ff5752bc93ac6c139b0e7955ddb21c1e
lpjfalcao <lpjfalcao@gmail.com>
2025-12-17 10:11:07 -0300

    Adicionando hover effect nos links da nav

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChildren } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import { MatIcon, MatIconModule } from '@angular/material/icon';
 
@@ -10,5 +10,20 @@ import { MatIcon, MatIconModule } from '@angular/material/icon';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  @ViewChildren('itemLista') itensMenu!: any;
 
+  active(element: MouseEvent) {
+    this.itensMenu.forEach((el: any) => {
+      el.nativeElement.classList.remove('menu-active');
+      el.nativeElement.querySelector('.hover-border').classList.remove('hover-border-active');
+    });
+
+    const target = element.currentTarget as HTMLElement;
+    const hoverBorder = target.querySelector('.hover-border');
+
+    if (target) {
+      target.classList.add('menu-active');
+      hoverBorder?.classList.add('hover-border-active');
+    }    
+  }
 }
