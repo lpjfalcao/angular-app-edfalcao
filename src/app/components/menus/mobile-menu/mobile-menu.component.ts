4d7f0e2eb25699ffd481c881ab159d219b50c147
lpjfalcao <lpjfalcao@gmail.com>
2025-12-18 12:42:15 -0300

    Compenentizando o menu mobile

--- src/app/components/menus/mobile-menu/mobile-menu.component.ts
+++ src/app/components/menus/mobile-menu/mobile-menu.component.ts
@@ -1,18 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MobileMenuService } from './mobile-menu.service';
 import { MatListModule } from '@angular/material/list';
+import { MenuMobileViewModel } from '../../../viewModels/menuMobile.viewModel';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'mobile-menu',
   standalone: true,
-  imports: [MatListModule],
+  imports: [MatListModule, CommonModule],
   templateUrl: './mobile-menu.component.html',
   styleUrl: './mobile-menu.component.scss'
 })
 export class MobileMenuComponent {
-  constructor(public mobileMenuService: MobileMenuService) {}
+  @Input() menuItems: MenuMobileViewModel[] = [];
 
-    action(task: string) {
-      console.log('item list clicked');
+  constructor(public mobileMenuService: MobileMenuService) { }
+
+  ngOnInit() {
+    this.loadMenuItems();
+  }
+
+  action(url: string) {
+    console.log(`open ${url}`);
+  }
+
+  loadMenuItems() {
+    this.menuItems = [
+      { title: 'Home', url: '/home'  },
+      { title: 'Obras', url: '/obras' },
+      { title: 'Premiações', url: '/premiacoes' },
+      { title: 'Eventos', url: '/eventos'},
+      { title: 'Biografia', url: '/bio',  },
+      { title: 'Contato', url: '/contato' }
+    ];
   }
 }
---
+++ src/app/viewModels/menuMobile.viewModel.ts
@@ -0,0 +1,4 @@
+export class MenuMobileViewModel {
+    title!: string;
+    url!: string;
+}
