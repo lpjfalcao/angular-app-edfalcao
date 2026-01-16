7601ac3b4e0bee04a783c520d156b80f15982162
Luã Falcão <lpjfalcao@gmail.com>
2026-01-16 15:57:51 -0300

    Adicionando serviço para navegar entre telas

---
+++ src/app/shared/services/common.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommonService {
+
+  constructor(private router: Router) { }
+
+  goTo(path: string) {
+    this.router.navigate([path]);
+  }
+}
